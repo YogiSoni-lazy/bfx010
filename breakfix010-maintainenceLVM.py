6a07b3590f622dafa3a489a13ade128fb994a690
YogiSoni-lazy <59290335+YogiSoni-lazy@users.noreply.github.com>
2023-10-17 22:18:22 +0530

    Update breakfix010-maintainenceLVM.py

--- breakfix010-maintainenceLVM.py
+++ breakfix010-maintainenceLVM.py
@@ -35,9 +35,14 @@ class Breakfix010Maintainencelvm(Default):
                 mkdir /mnt/data;
                 mount /dev/vg01/lv01 /mnt/data;
                 echo '/dev/vg01/lv01 /mnt/data xfs defaults 0 0' | sudo  tee -a /etc/fstab;
-                sed  -i '130i      filter=["r|.*/|"]' /etc/lvm/lvm.conf;
-                dracut -f  &>> /dev/null;
-                touch /var/tmp/.kc1;
+                (echo o # Create a new empty DOS partition table
+                echo n # Add a new partition
+                echo p # Primary partition
+                echo 1 # Partition number
+                echo   # First sector (Accept default: 1)
+                echo   # Last sector (Accept default: varies)
+                echo w # Write changes
+                ) | sudo fdisk /dev/vdb
                 ''',
                 shell=True,
             ),
