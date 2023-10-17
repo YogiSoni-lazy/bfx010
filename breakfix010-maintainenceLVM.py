47f84659323cb0c65623c714e64d52ef8250a28e
YogiSoni-lazy <59290335+YogiSoni-lazy@users.noreply.github.com>
2023-10-17 22:14:30 +0530

    Update breakfix010-maintainenceLVM.py

--- breakfix010-maintainenceLVM.py
+++ breakfix010-maintainenceLVM.py
@@ -4,15 +4,44 @@
 # All rights reserved.
 # No warranty, explicit or implied, provided.
 
-from labs.common import userinterface
+from labs import labconfig
 from labs.grading import Default
+from labs.common import steps, labtools, userinterface
 
+SKU = labconfig.get_course_sku().upper()
+
+_targets = ["servera"]
+_servera = "servera"
 
 class Breakfix010Maintainencelvm(Default):
     __LAB__ = "breakfix010-maintainenceLVM"
 
     def start(self):
-        items = []
+        items = [
+            {
+                "label": "Checking lab systems",
+                "task": labtools.check_host_reachable,
+                "hosts": _targets,
+                "fatal": True,
+            },
+            steps.run_command(
+                label="Configuring " + _servera,
+                hosts=[_servera],
+                command='''
+                pvcreate /dev/vdb;
+                vgcreate vg01 /dev/vdb;
+                lvcreate -L 800M -n lv01 vg01;
+                mkfs.xfs /dev/vg01/lv01;
+                mkdir /mnt/data;
+                mount /dev/vg01/lv01 /mnt/data;
+                echo '/dev/vg01/lv01 /mnt/data xfs defaults 0 0' | sudo  tee -a /etc/fstab;
+                sed  -i '130i      filter=["r|.*/|"]' /etc/lvm/lvm.conf;
+                dracut -f  &>> /dev/null;
+                touch /var/tmp/.kc1;
+                ''',
+                shell=True,
+            ),
+        ]
         userinterface.Console(items).run_items(action="Starting")
 
     def grade(self):
